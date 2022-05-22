7ed5fbbbb15f16217d56c4c034af139e0ff2ba02
Neil-Chen <v009008777@hotmail.com>
2022-05-22 18:59:43 +0800

    Init

---
+++ README.md
@@ -0,0 +1,14 @@
+# Fuck Stupid Timewaster SiShi (实屎)
+## Quick Start
+1. 修改 `final.txt` . 删除所有开刷 **之前** 的答案 (对照 pdf 或 docx 文件)
+2. `pip3 install pyperclip`
+3. `python3 main.py` 
+4. 输入题目个数
+5. 此时答案会自动复制到剪切板
+6. 打开浏览器控制台( `F12` or `Ctrl + Shift + I` )，粘贴命令
+7. 循环 `4 - 6` 步
+
+## Known Issues
+1. 所有 ①②③④ 都不能识别，需要手动点选，（有选项提示）
+2. 所有答案重复的都只能选到第一个，后面的要手动点选
+3. 中途退出后需要手动修改 `final.txt`
---
+++ main.py
@@ -0,0 +1,39 @@
+# read final.txt line by line and add to a list
+def read_file():
+    with open("final.txt", "r") as f:
+        lines = f.readlines()
+        return lines
+
+# use every line to construct a js command
+# i for time it takes to run the command
+# pop lines everytime it is called
+# command example
+# document.evaluate("//div[normalize-space(text())='TEXT to substitute']", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();
+def copyCommandToClipboard(i, lines):
+    commands = []
+    commands.append("try {")
+    for line in lines:
+        while i > 0:
+            line = lines.pop(0)
+            # if ① or ② or ③ or ④ in line, change to console.warn
+            if '①' in line or '②' in line or '③' in line or '④' in line:
+                command = "console.log('" + line.strip() + "');"
+            else:
+                command = "document.evaluate(\"//div[normalize-space(text())='" + line.strip() + "']\", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();"
+            commands.append(command)
+            i -= 1
+    commands.append("} catch (e) {}")
+    pyperclip.copy('\n'.join(commands))
+    print('\n'.join(commands))
+    print("Copied to clipboard")
+    print("------------------------------------------------------")
+
+
+
+if __name__ == "__main__":
+    import pyperclip
+    lines = read_file()
+    i = int(input("How many questions are there: "))
+    while i != -1:
+        copyCommandToClipboard(i, lines)
+        i = int(input("How many questions are there, -1 to exit: "))
