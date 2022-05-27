03f37b07b34b3514f0fb8c37a6c605adffcaa0a4
Neil-Chen <v009008777@hotmail.com>
2022-05-27 21:21:21 +0800

    rm blank and change " to `

--- main.py
+++ main.py
@@ -14,21 +14,21 @@ def read_file():
 def copyCommandToClipboard(i, lines):
     commands = []
     commands.append('let x = document.querySelectorAll("div.flex-fill")')
-    commands.append("try {")
+    # commands.append("try {")
     for line in lines:
         while i > 0:
             line = lines.pop(0)
             # if ① or ② or ③ or ④ in line, change to console.warn
             if '①' in line or '②' in line or '③' in line or '④' in line:
                 # command = "console.log('" + line.strip() + "');"
-                command = 'Array.from(x).find((node) => node.innerText == "' + line.strip() + '\\n").click();'
+                command = 'Array.from(x).find((node) => node.innerText == `' + line.strip() + '\\n`).click();'
             else:
-                command = 'Array.from(x).find((node) => node.innerText == "' + line.strip() + '").click();'
+                command = 'Array.from(x).find((node) => node.innerText == `' + line.strip() + '`).click();'
             #     command = "document.evaluate(`//div[normalize-space(text())='" + line.strip(
             #     ) + "']`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();"
             commands.append(command)
             i -= 1
-    commands.append("} catch (e) {}")
+    # commands.append("} catch (e) {}")
     pyperclip.copy('\n'.join(commands))
     print('\n'.join(commands))
     print("Copied to clipboard")
