6b76b47da63e8edc8ab274fbabfda5208d2e5766
Neil-Chen <v009008777@hotmail.com>
2022-05-27 21:05:56 +0800

    add support for 1234 change from xpath to querySelectorAll

--- main.py
+++ main.py
@@ -13,16 +13,19 @@ def read_file():
 
 def copyCommandToClipboard(i, lines):
     commands = []
+    commands.append('let x = document.querySelectorAll("div.flex-fill")')
     commands.append("try {")
     for line in lines:
         while i > 0:
             line = lines.pop(0)
             # if ① or ② or ③ or ④ in line, change to console.warn
             if '①' in line or '②' in line or '③' in line or '④' in line:
-                command = "console.log('" + line.strip() + "');"
+                # command = "console.log('" + line.strip() + "');"
+                command = 'Array.from(x).find((node) => node.innerText == "' + line.strip() + '\\n").click();'
             else:
-                command = "document.evaluate(`//div[normalize-space(text())='" + line.strip(
-                ) + "']`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();"
+                command = 'Array.from(x).find((node) => node.innerText == "' + line.strip() + '").click();'
+            #     command = "document.evaluate(`//div[normalize-space(text())='" + line.strip(
+            #     ) + "']`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click();"
             commands.append(command)
             i -= 1
     commands.append("} catch (e) {}")
@@ -32,7 +35,7 @@ def copyCommandToClipboard(i, lines):
     print("------------------------------------------------------")
 
 def saveRestLines(lines):
-    with open("rest.txt", "w") as f:
+    with open("final.txt", "w") as f:
         f.writelines(lines)
 
 if __name__ == "__main__":
